e0c38a5a571ecef44379098961c1fc9051f3ed8e
Rimac48 <mattia.bertozzi@live.it>
2021-05-17 11:20:32 +0200

    Primo

---
+++ Bertozzi.Mattia.4H.ValEsp/Models/Pila.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bertozzi.Mattia._4H.ValEsp.Models
+{
+    class Pila
+    {
+        int lunghezza=0;
+        List<string> pila;
+
+        //crea la lista pila
+        public Pila()
+        {
+            pila = new();
+        }
+
+        //inserisce il numero dell'espressione nella pila
+        public void Push(string num)
+        {
+            pila.Add(num);
+            lunghezza++;
+        }
+
+        //restituisce il primo numero in fondo alla pila e rimuove uno spazio
+        public string Pop()
+        {
+            if(lunghezza!=0)
+            {
+                string num = pila[lunghezza - 1];
+                pila.RemoveAt(lunghezza - 1);
+                lunghezza--;
+                return num;
+            }
+            return "";
+        }
+    }
+}
---
+++ Bertozzi.Mattia.4H.ValEsp/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Bertozzi.Mattia._4H.ValEsp.Models;
+
+namespace Bertozzi.Mattia._4H.ValEsp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] espressione = {"3","4","5","*","+"};//(5*4)+3
+
+            Pila p = new Pila();
+
+            Console.WriteLine(Operazione(espressione));
+        }
+        
+        static int Operazione(string[]espressione)
+        {
+            Pila pila = new Pila();
+            for(int i=0;i<espressione.Length;i++)
+            {
+                //rileva se epressione[i] è un numero,se lo è, esegue il push(inserisce nella pila),altrimenti va a fare i calcoli
+                if (int.TryParse(espressione[i],out int n))
+                {
+                    pila.Push(espressione[i]);
+                }
+                else
+                {
+                    int ris = FaiCalcolo(pila,espressione[i]);//espressione conterrà l'operando
+                    pila.Push(ris.ToString());
+                }
+            }
+            return Convert.ToInt32(pila.Pop());
+        }
+
+        static int FaiCalcolo(Pila pila,string espressione)
+        {
+            int Risultato = 0;
+
+            int op1 = Convert.ToInt32(pila.Pop());
+            int op2 = Convert.ToInt32(pila.Pop());
+
+            switch (espressione)
+            {
+                case "+":
+                    Risultato = op1 + op2;
+                    break;
+                case "-":
+                    Risultato = op1 - op2;
+                    break;
+                case "*":
+                    Risultato = op1 * op2;
+                    break;
+                case "/":
+                    Risultato = op1 / op2;
+                    break;
+            }
+
+            return Risultato;
+        }
+    }
+}
